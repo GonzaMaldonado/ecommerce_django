79aefd86fbaa228fc81b1d91d9eeb6f1e0a7699e
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-11 22:06:14 -0300

    feat: iniciando html en login

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import login, register
 
-urlpatterns = []
+urlpatterns = [
+  path('login/', login, name="login"),
+  path('register/', register, name="register"),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+  return render(request, 'users/login.html')
+
+def register(request):
+  pass
+  #return render(request, 'users/register.html')
+
+def logout(request):
+  pass
