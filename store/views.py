0c0b96a23cdca3985515eb50862b23d923a5299e
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-11 21:35:47 -0300

    feat: añadiendo rutas de store

--- store/views.py
+++ store/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, 'store/store.html')
+
+def cart(request):
+  return render(request, 'store/cart.html')
+
+def checkout(request):
+  return render(request, 'store/checkout.html')
