7f70446ab200a355cfae722970c5421a925af3e1
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-11 22:32:10 -0300

    feat: migrando modelos de store

--- store/models.py
+++ store/models.py
@@ -83,5 +83,9 @@ class ShippingAddress(models.Model):
 	zipcode = models.CharField(max_length=200, null=False)
 	date_added = models.DateTimeField(auto_now_add=True)
 
+	class Meta:
+		verbose_name_plural = "Shipping Addresses"
+		ordering = ['-date_added']
+
 	def __str__(self):
 		return self.address
