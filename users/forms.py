fdc08781335c9b22f792cc94608d53ac32deec7c
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-12 15:28:45 -0300

    feat: cambiando login

--- users/forms.py
+++ users/forms.py
@@ -10,10 +10,10 @@ class RegisterForm(UserCreationForm):
 
   def __init__(self, *args, **kwargs):
     super(RegisterForm, self).__init__(*args, **kwargs)
-    self.fields['username'].widget.attrs['class'] = 'form-control text-dark text-sm rounded-lg d-block w-100 p-2.5 placeholder-gray-400'
-    self.fields['email'].widget.attrs['class'] = 'form-control text-dark text-sm rounded-lg d-block w-100 p-2.5 placeholder-gray-400'
-    self.fields['password1'].widget.attrs['class'] = 'form-control text-dark text-sm rounded-lg d-block w-100 p-2.5 placeholder-gray-400'
-    self.fields['password2'].widget.attrs['class'] = 'form-control text-dark text-sm rounded-lg d-block w-100 p-2.5 placeholder-gray-400'
+    self.fields['username'].widget.attrs['class'] = 'form-control text-dark text-sm rounded-lg d-block w-100 p-1 placeholder-gray-400'
+    self.fields['email'].widget.attrs['class'] = 'form-control text-dark text-sm rounded-lg d-block w-100 p-1 placeholder-gray-400'
+    self.fields['password1'].widget.attrs['class'] = 'form-control text-dark text-sm rounded-lg d-block w-100 p-1 placeholder-gray-400'
+    self.fields['password2'].widget.attrs['class'] = 'form-control text-dark text-sm rounded-lg d-block w-100 p-1 placeholder-gray-400'
     self.fields['username'].widget.attrs['placeholder'] = 'Username'
     self.fields['email'].widget.attrs['placeholder'] = 'Email'
     self.fields['password1'].widget.attrs['placeholder'] = 'Password'
