a1fe06f2b25de8c4926a5d0cbfb5fb9b7207aec7
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-11-12 16:20:33 -0300

    feat: listando productos en cart

--- store/views.py
+++ store/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 
 def home(request):
   products = Product.objects.all()
   return render(request, 'store/store.html', {'products': products})
 
+
 def cart(request):
-  return render(request, 'store/cart.html')
+  if request.user.is_authenticated:
+    order, created = Order.objects.get_or_create(user=request.user, complete=False)
+    items = order.orderitems.all()
+  else:
+    items = []
+    order = {'get_cart_total': 0, 'get_cart_items': 0}
+  return render(request, 'store/cart.html', {'items': items, 'order': order})
+
 
 def checkout(request):
-  return render(request, 'store/checkout.html')
+  if request.user.is_authenticated:
+    order, created = Order.objects.get_or_create(user=request.user, complete=False)
+    items = order.orderitems.all()
+  else:
+    items = []
+    order = {'get_cart_total': 0, 'get_cart_items': 0}
+  return render(request, 'store/checkout.html', {'items': items, 'order': order})
